a9f57828a150e108240e0bb7c1b45311b5a76ea2
Никита Королев <nickkorol1994@gmail.com>
2019-11-20 17:49:24 +0300

    Initial Commit

---
+++ KVO+KVCTest_PDP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KVO+KVCTest_PDP
+//
+//  Created by Nikita Korolev on 20/11/2019.
+//  Copyright © 2019 Никита Королев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
